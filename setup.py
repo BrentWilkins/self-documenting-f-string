62adeb2a1453a22f9e673a3cb1983f6a87546777
Brent Wilkins <brent@wilkins.in>
2024-01-08 18:13:30 -0700

    Refactored a bit and fixed return value from FStringChecker.run

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup
 
 setup(
-    name='self-documenting-f-string',
-    version='0.1.1',
+    name='flake8-self-documenting-f-strings',
+    version='0.1.2',
     description='flake8 plugin to run pycodecheck but ignore their incorrect self-documenting f-string errors in Python 3.12',
     author='Brent Wilkins',
     author_email='brent@wilkins.in',
     url='https://github.com/brentwilkins/self-documenting-f-string',
-    packages=['self_documenting_f_string'],
+    packages=['self_documenting_f_strings'],
     entry_points={
         'flake8.extension': [
-            'E = self_documenting_f_string.f_string_rule:CustomChecker',
+            'E = self_documenting_f_strings:FStringChecker',
         ],
     },
 )
